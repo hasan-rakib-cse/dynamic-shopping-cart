a93d9a196dc18f6db5c40ca2b09606727b8639f7
MD. RAKIB HASAN <hasanrakibcse@gmail.com>
2023-09-12 14:59:25 +0600

    subTotal calculation done

--- main.js
+++ main.js
@@ -30,29 +30,43 @@
 
 
 function handleProductChange(product, isIncrease) {
-  const productInput = document.getElementById(product + 'Count');
-  const productCount = parseInt(productInput.value);
-  let productNewCount = productCount;
+    const productInput = document.getElementById(product + 'Count');
+    const productCount = parseInt(productInput.value);
+    let productNewCount = productCount;
 
-  if(isIncrease == true) {
-    productNewCount = productCount + 1;
-  }
-  if(isIncrease == false && productCount > 0) {
-    productNewCount = productCount - 1;
-  }
+    if(isIncrease == true) {
+        productNewCount = productCount + 1;
+    }
+    if(isIncrease == false && productCount > 1) {
+        productNewCount = productCount - 1;
+    }
 
-  productInput.value = productNewCount;
+    productInput.value = productNewCount;
 
-  let productTotal = 0;
-  if(product == 'phone') {
-    productTotal = (productNewCount * 200.20).toFixed(2);
-  }
-  if(product == 'case') {
-    productTotal = (productNewCount * 50.25).toFixed(2);
-  }
+    let productTotal = 0;
+    if(product == 'phone') {
+        productTotal = (productNewCount * 200.20).toFixed(2);
+    }
+    if(product == 'case') {
+        productTotal = (productNewCount * 50.25).toFixed(2);
+    }
 
-  document.getElementById(product + 'Total').innerText = productTotal;
+    document.getElementById(product + 'Total').innerText = productTotal;
+    calculateTotal();
 }
+
+
+function calculateTotal() {
+  const phoneInput = document.getElementById('phoneCount');
+  const phoneCount = parseInt(phoneInput.value);
+
+  const caseInput = document.getElementById('caseCount');
+  const caseCount = parseInt(caseInput.value);
+
+  const totalPrice = (phoneCount * 200.20 + caseCount * 50.25).toFixed(2);
+  document.getElementById('subTotal').innerText = totalPrice;
+}
+
 
 document.getElementById('phoneIncrease').addEventListener('click', function() {
   handleProductChange('phone', true);
