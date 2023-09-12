0f257a9fad5cf61e09c00cf4c3e44713187e3e6a
MD. RAKIB HASAN <hasanrakibcse@gmail.com>
2023-09-12 12:28:06 +0600

    cariable and function name changed from all places

--- main.js
+++ main.js
@@ -1,42 +1,40 @@
-// document.getElementById('increaseBtn_1').addEventListener('click', function() {
-//   const caseInput = document.getElementById('inputItem_1');
+// document.getElementById('phoneIncrease').addEventListener('click', function() {
+//   const caseInput = document.getElementById('phoneCount');
 //   const caseCount = parseInt(caseInput.value);
 //   const caseNewCount = caseCount + 1;
 //   caseInput.value = caseNewCount;
 //   const caseTotal = caseNewCount * 50;
-//   document.getElementById('caseTotal_1').innerText = caseTotal;
+//   document.getElementById('phoneTotal').innerText = caseTotal;
 // })
 
-// document.getElementById('decreaseBtn_1').addEventListener('click', function() {
-//   const caseInput = document.getElementById('inputItem_1');
+// document.getElementById('phoneDecrease').addEventListener('click', function() {
+//   const caseInput = document.getElementById('phoneCount');
 //   const caseCount = parseInt(caseInput.value);
 //   const caseNewCount = caseCount - 1;
 //   caseInput.value = caseNewCount;
 //   const caseTotal = caseNewCount * 50;
-//   document.getElementById('caseTotal_1').innerHTML = caseTotal;
+//   document.getElementById('phoneTotal').innerHTML = caseTotal;
 // })
 
-document.getElementById('increaseBtn_1').addEventListener('click', function() {
+document.getElementById('phoneIncrease').addEventListener('click', function() {
   handleProducrChange(true);
 })
 
-document.getElementById('decreaseBtn_1').addEventListener('click', function() {
+document.getElementById('phoneDecrease').addEventListener('click', function() {
   handleProducrChange(false)
 })
 
 function handleProducrChange(isIncrease) {
-  const caseInput = document.getElementById('inputItem_1');
-  const caseCount = parseInt(caseInput.value);
-  let caseNewCount = caseCount;
-
+  const phoneInput = document.getElementById('phoneCount');
+  const phoneCount = parseInt(phoneInput.value);
+  let phoneNewCount = phoneCount;
   if(isIncrease == true) {
-    caseNewCount = caseCount + 1;
+    phoneNewCount = phoneCount + 1;
   }
-  if(isIncrease == false) {
-    caseNewCount = caseCount - 1;
+  if(isIncrease == false && phoneCount > 1) {
+    phoneNewCount = phoneCount - 1;
   }
-
-  caseInput.value = caseNewCount;
-  const caseTotal = caseNewCount * 50;
-  document.getElementById('caseTotal_1').innerHTML = caseTotal;
+  phoneInput.value = phoneNewCount;
+  const phoneTotal = phoneNewCount * 50;
+  document.getElementById('phoneTotal').innerHTML = phoneTotal;
 }
