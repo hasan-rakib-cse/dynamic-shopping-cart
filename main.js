e0b274cebe6d56a8f66e60d1cb5271cde931afaa
MD. RAKIB HASAN <hasanrakibcse@gmail.com>
2023-09-12 16:29:18 +0600

    all calculation done

--- main.js
+++ main.js
@@ -1,70 +1,52 @@
-// function handleProducrChange(isIncrease) {
-//   const phoneInput = document.getElementById('phoneCount');
-//   const phoneCount = parseInt(phoneInput.value);
-//   let phoneNewCount = phoneCount;
-//   if(isIncrease == true) {
-//     phoneNewCount = phoneCount + 1;
-//   }
-//   if(isIncrease == false && phoneCount > 1) {
-//     phoneNewCount = phoneCount - 1;
-//   }
-//   phoneInput.value = phoneNewCount;
-//   const phoneTotal = (phoneNewCount * 200.20).toFixed(2);
-//   document.getElementById('phoneTotal').innerText = phoneTotal;
-// }
-
-// function handleCaseChange(isIncrease) {
-//   const caseInput = document.getElementById('caseCount');
-//   const caseCount = parseInt(caseInput.value);
-//   let caseNewCount = caseCount;
-//   if(isIncrease == true) {
-//     caseNewCount = caseCount + 1;
-//   }
-//   if(isIncrease == false && caseCount > 1) {
-//     caseNewCount = caseCount - 1;
-//   }
-//   caseInput.value = caseNewCount;
-//   const caseTotal = (caseNewCount * 50.25).toFixed(2);
-//   document.getElementById('caseTotal').innerText = caseTotal;
-// }
-
-
 function handleProductChange(product, isIncrease) {
-    const productInput = document.getElementById(product + 'Count');
-    const productCount = parseInt(productInput.value);
-    let productNewCount = productCount;
+  const productInput = document.getElementById(product + 'Count');
+  const productCount = getInputValue(product);
+  let productNewCount = productCount;
 
-    if(isIncrease == true) {
-        productNewCount = productCount + 1;
-    }
-    if(isIncrease == false && productCount > 1) {
-        productNewCount = productCount - 1;
-    }
+  if(isIncrease == true) {
+      productNewCount = productCount + 1;
+  }
+  if(isIncrease == false && productCount > 1) {
+      productNewCount = productCount - 1;
+  }
 
-    productInput.value = productNewCount;
+  productInput.value = productNewCount;
 
-    let productTotal = 0;
-    if(product == 'phone') {
-        productTotal = (productNewCount * 200.20).toFixed(2);
-    }
-    if(product == 'case') {
-        productTotal = (productNewCount * 50.25).toFixed(2);
-    }
+  let productTotal = 0;
+  if(product == 'phone') {
+      productTotal = (productNewCount * 200.20).toFixed(2);
+  }
+  if(product == 'case') {
+      productTotal = (productNewCount * 50.25).toFixed(2);
+  }
 
-    document.getElementById(product + 'Total').innerText = productTotal;
-    calculateTotal();
+  document.getElementById(product + 'Total').innerText = productTotal;
+  
+  calculateTotal();
 }
 
 
 function calculateTotal() {
-  const phoneInput = document.getElementById('phoneCount');
-  const phoneCount = parseInt(phoneInput.value);
+  const phoneCount = getInputValue('phone');
+  const caseCount = getInputValue('case');
 
-  const caseInput = document.getElementById('caseCount');
-  const caseCount = parseInt(caseInput.value);
+  // Sub Total
+  const subTotal = (phoneCount * 200.20 + caseCount * 50.25).toFixed(2);
+  document.getElementById('subTotal').innerText = subTotal;
 
-  const totalPrice = (phoneCount * 200.20 + caseCount * 50.25).toFixed(2);
-  document.getElementById('subTotal').innerText = totalPrice;
+  // Tax
+  const totalTax = (subTotal * .05).toFixed(2);
+  document.getElementById('tax').innerText = totalTax;
+
+  // Grand Total
+  const grandTotal = (parseFloat(subTotal + totalTax)).toFixed(2)
+  document.getElementById('total').innerText = grandTotal;
+}
+
+function getInputValue(product) {
+  const productInput = document.getElementById(product + 'Count');
+  const productCount = parseInt(productInput.value);
+  return productCount;
 }
 
 
