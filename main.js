bd60e383a52796e84da1e45063741b2418b2152b
MD. RAKIB HASAN <hasanrakibcse@gmail.com>
2023-09-12 11:40:19 +0600

    commit-2

--- main.js
+++ main.js
@@ -7,3 +7,11 @@ document.getElementById('increaseBtn_1').addEventListener('click', function() {
   document.getElementById('caseTotal_1').innerText = caseTotal;
 })
 
+document.getElementById('decreaseBtn_1').addEventListener('click', function() {
+  const caseInput = document.getElementById('inputItem_1');
+  const caseCount = parseInt(caseInput.value);
+  const caseNewCount = caseCount - 1;
+  caseInput.value = caseNewCount;
+  const caseTotal = caseNewCount * 50;
+  document.getElementById('caseTotal_1').innerHTML = caseTotal;
+})
