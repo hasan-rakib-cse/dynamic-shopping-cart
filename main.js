7340f42ea8849b3259d26a5b36de786b7b2365a8
MD. RAKIB HASAN <hasanrakibcse@gmail.com>
2023-09-12 11:50:00 +0600

    for increase and decrease item only 1 flag is added

--- main.js
+++ main.js
@@ -1,17 +1,42 @@
+// document.getElementById('increaseBtn_1').addEventListener('click', function() {
+//   const caseInput = document.getElementById('inputItem_1');
+//   const caseCount = parseInt(caseInput.value);
+//   const caseNewCount = caseCount + 1;
+//   caseInput.value = caseNewCount;
+//   const caseTotal = caseNewCount * 50;
+//   document.getElementById('caseTotal_1').innerText = caseTotal;
+// })
+
+// document.getElementById('decreaseBtn_1').addEventListener('click', function() {
+//   const caseInput = document.getElementById('inputItem_1');
+//   const caseCount = parseInt(caseInput.value);
+//   const caseNewCount = caseCount - 1;
+//   caseInput.value = caseNewCount;
+//   const caseTotal = caseNewCount * 50;
+//   document.getElementById('caseTotal_1').innerHTML = caseTotal;
+// })
+
 document.getElementById('increaseBtn_1').addEventListener('click', function() {
-  const caseInput = document.getElementById('inputItem_1');
-  const caseCount = parseInt(caseInput.value);
-  const caseNewCount = caseCount + 1;
-  caseInput.value = caseNewCount;
-  const caseTotal = caseNewCount * 50;
-  document.getElementById('caseTotal_1').innerText = caseTotal;
+  handleProducrChange(true);
 })
 
 document.getElementById('decreaseBtn_1').addEventListener('click', function() {
+  handleProducrChange(false)
+})
+
+function handleProducrChange(isIncrease) {
   const caseInput = document.getElementById('inputItem_1');
   const caseCount = parseInt(caseInput.value);
-  const caseNewCount = caseCount - 1;
+  let caseNewCount = caseCount;
+
+  if(isIncrease == true) {
+    caseNewCount = caseCount + 1;
+  }
+  if(isIncrease == false) {
+    caseNewCount = caseCount - 1;
+  }
+
   caseInput.value = caseNewCount;
   const caseTotal = caseNewCount * 50;
   document.getElementById('caseTotal_1').innerHTML = caseTotal;
-})
+}
